5823eaa4000feaad479f9b518c230fc32e4991e3
Tieu Ngoc <thaingoctieu2002@gmail.com>
2023-04-16 09:57:40 +0700

    building user context...

--- src/store/DeviceReducer.js
+++ src/store/DeviceReducer.js
@@ -1,5 +1,8 @@
-import { SET_TEMP, SET_HUMID, SET_LIGHT, SET_FAN, 
-    SET_DOOR, SER_THEFT, SET_FIRE, ADD_FAN, ADD_LIGHT, SET_THEFT } from "./Constant"
+import {
+    SET_TEMP, SET_HUMID, SET_LIGHT, SET_FAN,
+    SET_DOOR, SER_THEFT, SET_FIRE, ADD_FAN, ADD_LIGHT, SET_THEFT
+} from "./Constant"
+import * as action from './DeviceAction'
 
 // MQTT initState
 const initState = {
@@ -11,7 +14,7 @@ const initState = {
             id: 1,
             checked: false,
         },
-        
+
     ],
     fan: 0,
     fans: [
@@ -19,11 +22,11 @@ const initState = {
             id: 11,
             checked: false,
         },
-        
+
     ],
     door: 4,
     theft: 0,
-    fire: 0, 
+    fire: 0,
 }
 
 function Reducer(state, action) {
@@ -33,13 +36,36 @@ function Reducer(state, action) {
                 ...state,
                 temp: action.payload
             }
-            break
         case SET_HUMID:
             return {
                 ...state,
                 humid: action.payload
             }
-            break
+        case SET_FAN:
+            return {
+                ...state,
+                fan: action.payload
+            }
+        case SET_LIGHT:
+            return {
+                ...state,
+                light: action.payload
+            }
+        case SET_DOOR:
+            return {
+                ...state,
+                door: action.payload
+            }
+        case SET_FIRE:
+            return {
+                ...state,
+                fire: action.payload
+            }
+        case SET_THEFT:
+            return {
+                ...state,
+                theft: action.payload,
+            }
         default:
             throw new Error('Invalid action!')
     }
